347b784cc49300a2d645ccf7c18582063cc01b00
magnusjensaas <magnusjensaas@gmail.com>
2020-02-13 23:04:26 +0100

    Added 3 new functions to the script.

--- cisco/dna-center/POST-request-auth-token.py
+++ cisco/dna-center/POST-request-auth-token.py
@@ -18,3 +18,27 @@ def get_auth_token(controller_ip=DNAC, username=DNAC_USER, password=DNAC_PASSWOR
         "token": token
     }
 
+
+
+def create_url(path, controller_ip=DNAC):
+
+    return "https://%s:%s/api/v1/%s" % (controller_ip, DNAC_PORT, path)
+
+
+
+def get_url(url):
+
+    url = create_url(path=url)
+    print (url)
+    token = get_auth_token()
+    headers = {"X-auth-token" : token["token"]}
+    try:
+        response = requests.get(url, headers = headers, verify = False)
+    except requests.exceptions.requestException as cerror:
+        print ("Error processing request", cerror)
+        sys.exit(1)
+
+    return response.json()
+
+def list_network_devices():
+    return get_url("network-device")
