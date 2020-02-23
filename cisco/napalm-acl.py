46375f8c193d3716cc94d9702d054063838fe979
magnusjensaas <magnusjensaas@gmail.com>
2020-02-23 22:36:33 +0100

    Added if statement that compares uploaded config to running config and discards the config if the uploaded configuration is already in use.

--- cisco/napalm-acl.py
+++ cisco/napalm-acl.py
@@ -6,8 +6,23 @@ driver = napalm.get_network_driver("ios")
 device = driver("192.168.122.72", "cisco", "cisco")
 device.open()
 
-#Prints connecting message, uploads .cfg file, commits the file to the running config and closes the connection.
+#Prints connecting message and uploads .cfg file
 print ("Connecting to 192.168.122.72")
 device.load_merge_candidate(filename="ACL1.cfg")
-device.commit_config()
+
+#Checks if config file has differencing config from running config
+diffs = device.compare_config()
+
+
+#Commits the config to running if the running config does not contatin the loaded config and discards if config is alreadt in place.
+if len(diffs) > 0:
+    print (diffs)
+    device.commit_config()
+else:
+    print ("This configuration is already in use")
+    device.discard_config()
+
+#Closes connection to device.
 device.close()
+
+
