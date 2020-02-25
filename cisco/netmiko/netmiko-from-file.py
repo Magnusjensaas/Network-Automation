2032b33f4af770bd7553861c34984c8289900ef4
magnusjensaas <magnusjensaas@gmail.com>
2020-02-25 02:13:22 +0100

    Added multi device support from file

--- cisco/netmiko/netmiko-from-file.py
+++ cisco/netmiko/netmiko-from-file.py
@@ -3,18 +3,20 @@ from netmiko import ConnectHandler
 with open("command_file") as f:
     commands_to_send = f.read().splitlines()
 
-ios_devices = {
-    "device_type": "cisco_ios",
-    "ip": "192.168.122.72",
-    "username": "cisco",
-    "password": "cisco"
-}
-
-all_devices = [ios_devices]
+with open("devices_file") as f:
+    devices_list = f.read().splitlines()
 
 
-for devices in all_devices:
-    net_connect = ConnectHandler(**devices)
+for devices in devices_list:
+    print ("Connecting to device " + devices)
+    device_ip = devices
+    ios_device = {
+        "device_type": "cisco_ios",
+        "ip": device_ip,
+        "username": "cisco",
+        "password": "cisco"
+    }
+    net_connect = ConnectHandler(**ios_device)
     output = net_connect.send_config_set(commands_to_send)
     print(output)
 
