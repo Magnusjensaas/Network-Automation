9b650d91c9d96e4591fb29327adbacba569c0ea7
magnusjensaas <magnusjensaas@gmail.com>
2020-03-29 21:26:21 +0200

    added SimpleController

--- design/MVC/controller.py
+++ design/MVC/controller.py
@@ -12,3 +12,21 @@ class UserController(ABC):
     @abstractmethod
     def get(self, id):
         raise NotImplementedError
+
+
+class SimpleController(UserController):
+    def create(self, username, email):
+        if "@" not in email:
+            self.view.update_display(False, "Email not valid")
+        else:
+            user = model.User(username, email)
+            result = user.store_user()
+            if result:
+                self.view.update_display(True, "User created!")
+
+    def get(self, id):
+        user = model.User.get_user(id)
+        if user:
+            self.view.update_display(True, user)
+        else:
+            self.view.update_display(False, f"User id {id} not found")
